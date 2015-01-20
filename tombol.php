f941f4525124550ffc5d553f63576b2dfa270f77
kaosada <kaosadandro@gmail.com>
2015-01-20 21:59:09 +0700

    Create tombol.php

---
+++ tombol.php
@@ -0,0 +1,154 @@
+<html>
+<body>
+
+<b>Modem 0</b><br/>
+<!--Untuk Start Service-->
+<a href="modem0.php" onclick="javascript:void window.open('modem0.php','1421757992183','width=400,height=200,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=0,top=0');return false;"><img src="img/1.png" alt="start service" height="24" width="24" /></a>
+<!--Untuk cek Pulsa-->
+<a href="cekpulsa/modem0.php" onclick="javascript:void window.open('cekpulsa/modem0.php','1421757992183','width=400,height=200,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=0,top=0');return false;"><img src="img/refresh.png" alt="cekpulsa" height="24" width="24" /></a>
+<!--Untuk Stop Service-->
+<!--<a href="cekpulsa/modem0.php" onclick="javascript:void window.open('cekpulsa/modem0.php','1421757992183','width=400,height=200,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=0,top=0');return false;"><img src="img/refresh.png" alt="cekpulsa" height="24" width="24" /></a>-->
+<br />
+<hr size=2px color="blue" />
+
+
+<b>Modem 1</b><br/>
+<!--Untuk Start Service-->
+<a href="modem1.php" onclick="javascript:void window.open('modem1.php','1421757992183','width=400,height=200,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=0,top=0');return false;"><img src="img/1.png" alt="start service" height="24" width="24" /></a>
+<!--Untuk cek Pulsa-->
+<a href="cekpulsa/modem1.php" onclick="javascript:void window.open('cekpulsa/modem1.php','1421757992183','width=400,height=200,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=0,top=0');return false;"><img src="img/refresh.png" alt="cekpulsa" height="24" width="24" /></a>
+<!--Untuk Stop Service-->
+<!--<a href="cekpulsa/modem0.php" onclick="javascript:void window.open('cekpulsa/modem0.php','1421757992183','width=400,height=200,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=0,top=0');return false;"><img src="img/refresh.png" alt="cekpulsa" height="24" width="24" /></a>-->
+<br />
+<hr size=2px color="blue" />
+
+
+<b>Modem 2</b><br/>
+<!--Untuk Start Service-->
+<a href="modem2.php" onclick="javascript:void window.open('modem2.php','1421757992183','width=400,height=200,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=0,top=0');return false;"><img src="img/1.png" alt="start service" height="24" width="24" /></a>
+<!--Untuk cek Pulsa-->
+<a href="cekpulsa/modem2.php" onclick="javascript:void window.open('cekpulsa/modem2.php','1421757992183','width=400,height=200,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=0,top=0');return false;"><img src="img/refresh.png" alt="cekpulsa" height="24" width="24" /></a>
+<!--Untuk Stop Service-->
+<!--<a href="cekpulsa/modem0.php" onclick="javascript:void window.open('cekpulsa/modem0.php','1421757992183','width=400,height=200,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=0,top=0');return false;"><img src="img/refresh.png" alt="cekpulsa" height="24" width="24" /></a>-->
+<br />
+<hr size=2px color="blue" />
+
+
+<b>Modem 3</b><br/>
+<!--Untuk Start Service-->
+<a href="modem3.php" onclick="javascript:void window.open('modem3.php','1421757992183','width=400,height=200,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=0,top=0');return false;"><img src="img/1.png" alt="start service" height="24" width="24" /></a>
+<!--Untuk cek Pulsa-->
+<a href="cekpulsa/modem3.php" onclick="javascript:void window.open('cekpulsa/modem3.php','1421757992183','width=400,height=200,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=0,top=0');return false;"><img src="img/refresh.png" alt="cekpulsa" height="24" width="24" /></a>
+<!--Untuk Stop Service-->
+<!--<a href="cekpulsa/modem0.php" onclick="javascript:void window.open('cekpulsa/modem0.php','1421757992183','width=400,height=200,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=0,top=0');return false;"><img src="img/refresh.png" alt="cekpulsa" height="24" width="24" /></a>-->
+<br />
+<hr size=2px color="blue" />
+
+
+<b>Modem 4</b><br/>
+<!--Untuk Start Service-->
+<a href="modem4.php" onclick="javascript:void window.open('modem4.php','1421757992183','width=400,height=200,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=0,top=0');return false;"><img src="img/1.png" alt="start service" height="24" width="24" /></a>
+<!--Untuk cek Pulsa-->
+<a href="cekpulsa/modem4.php" onclick="javascript:void window.open('cekpulsa/modem4.php','1421757992183','width=400,height=200,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=0,top=0');return false;"><img src="img/refresh.png" alt="cekpulsa" height="24" width="24" /></a>
+<!--Untuk Stop Service-->
+<!--<a href="cekpulsa/modem0.php" onclick="javascript:void window.open('cekpulsa/modem0.php','1421757992183','width=400,height=200,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=0,top=0');return false;"><img src="img/refresh.png" alt="cekpulsa" height="24" width="24" /></a>-->
+<br />
+<hr size=2px color="blue" />
+
+
+<b>Modem 5</b><br/>
+<!--Untuk Start Service-->
+<a href="modem5.php" onclick="javascript:void window.open('modem5.php','1421757992183','width=400,height=200,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=0,top=0');return false;"><img src="img/1.png" alt="start service" height="24" width="24" /></a>
+<!--Untuk cek Pulsa-->
+<a href="cekpulsa/modem5.php" onclick="javascript:void window.open('cekpulsa/modem5.php','1421757992183','width=400,height=200,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=0,top=0');return false;"><img src="img/refresh.png" alt="cekpulsa" height="24" width="24" /></a>
+<!--Untuk Stop Service-->
+<!--<a href="cekpulsa/modem0.php" onclick="javascript:void window.open('cekpulsa/modem0.php','1421757992183','width=400,height=200,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=0,top=0');return false;"><img src="img/refresh.png" alt="cekpulsa" height="24" width="24" /></a>-->
+<br />
+<hr size=2px color="blue" />
+
+
+<b>Modem 6</b><br/>
+<!--Untuk Start Service-->
+<a href="modem6.php" onclick="javascript:void window.open('modem6.php','1421757992183','width=400,height=200,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=0,top=0');return false;"><img src="img/1.png" alt="start service" height="24" width="24" /></a>
+<!--Untuk cek Pulsa-->
+<a href="cekpulsa/modem6.php" onclick="javascript:void window.open('cekpulsa/modem6.php','1421757992183','width=400,height=200,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=0,top=0');return false;"><img src="img/refresh.png" alt="cekpulsa" height="24" width="24" /></a>
+<!--Untuk Stop Service-->
+<!--<a href="cekpulsa/modem0.php" onclick="javascript:void window.open('cekpulsa/modem0.php','1421757992183','width=400,height=200,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=0,top=0');return false;"><img src="img/refresh.png" alt="cekpulsa" height="24" width="24" /></a>-->
+<br />
+<hr size=2px color="blue" />
+
+
+<b>Modem 7</b><br/>
+<!--Untuk Start Service-->
+<a href="modem7.php" onclick="javascript:void window.open('modem7.php','1421757992183','width=400,height=200,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=0,top=0');return false;"><img src="img/1.png" alt="start service" height="24" width="24" /></a>
+<!--Untuk cek Pulsa-->
+<a href="cekpulsa/modem7.php" onclick="javascript:void window.open('cekpulsa/modem7.php','1421757992183','width=400,height=200,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=0,top=0');return false;"><img src="img/refresh.png" alt="cekpulsa" height="24" width="24" /></a>
+<!--Untuk Stop Service-->
+<!--<a href="cekpulsa/modem0.php" onclick="javascript:void window.open('cekpulsa/modem0.php','1421757992183','width=400,height=200,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=0,top=0');return false;"><img src="img/refresh.png" alt="cekpulsa" height="24" width="24" /></a>-->
+<br />
+<hr size=2px color="blue" />
+
+
+<b>Modem 8</b><br/>
+<!--Untuk Start Service-->
+<a href="modem8.php" onclick="javascript:void window.open('modem8.php','1421757992183','width=400,height=200,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=0,top=0');return false;"><img src="img/1.png" alt="start service" height="24" width="24" /></a>
+<!--Untuk cek Pulsa-->
+<a href="cekpulsa/modem8.php" onclick="javascript:void window.open('cekpulsa/modem8.php','1421757992183','width=400,height=200,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=0,top=0');return false;"><img src="img/refresh.png" alt="cekpulsa" height="24" width="24" /></a>
+<!--Untuk Stop Service-->
+<!--<a href="cekpulsa/modem0.php" onclick="javascript:void window.open('cekpulsa/modem0.php','1421757992183','width=400,height=200,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=0,top=0');return false;"><img src="img/refresh.png" alt="cekpulsa" height="24" width="24" /></a>-->
+<br />
+<hr size=2px color="blue" />
+
+
+<b>Modem 9</b><br/>
+<!--Untuk Start Service-->
+<a href="modem9.php" onclick="javascript:void window.open('modem9.php','1421757992183','width=400,height=200,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=0,top=0');return false;"><img src="img/1.png" alt="start service" height="24" width="24" /></a>
+<!--Untuk cek Pulsa-->
+<a href="cekpulsa/modem9.php" onclick="javascript:void window.open('cekpulsa/modem9.php','1421757992183','width=400,height=200,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=0,top=0');return false;"><img src="img/refresh.png" alt="cekpulsa" height="24" width="24" /></a>
+<!--Untuk Stop Service-->
+<!--<a href="cekpulsa/modem0.php" onclick="javascript:void window.open('cekpulsa/modem0.php','1421757992183','width=400,height=200,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=0,top=0');return false;"><img src="img/refresh.png" alt="cekpulsa" height="24" width="24" /></a>-->
+<br />
+<hr size=2px color="blue" />
+
+
+<b>Modem 10</b><br/>
+<!--Untuk Start Service-->
+<a href="modem10.php" onclick="javascript:void window.open('modem10.php','1421757992183','width=400,height=200,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=0,top=0');return false;"><img src="img/1.png" alt="start service" height="24" width="24" /></a>
+<!--Untuk cek Pulsa-->
+<a href="cekpulsa/modem10.php" onclick="javascript:void window.open('cekpulsa/modem10.php','1421757992183','width=400,height=200,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=0,top=0');return false;"><img src="img/refresh.png" alt="cekpulsa" height="24" width="24" /></a>
+<!--Untuk Stop Service-->
+<!--<a href="cekpulsa/modem10.php" onclick="javascript:void window.open('cekpulsa/modem10.php','1421757992183','width=400,height=200,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=0,top=0');return false;"><img src="img/refresh.png" alt="cekpulsa" height="24" width="24" /></a>-->
+<br />
+<hr size=2px color="blue" />
+
+
+<b>Modem 11</b><br/>
+<!--Untuk Start Service-->
+<a href="modem11.php" onclick="javascript:void window.open('modem11.php','1421757992183','width=400,height=200,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=0,top=0');return false;"><img src="img/1.png" alt="start service" height="24" width="24" /></a>
+<!--Untuk cek Pulsa-->
+<a href="cekpulsa/modem11.php" onclick="javascript:void window.open('cekpulsa/modem11.php','1421757992183','width=400,height=200,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=0,top=0');return false;"><img src="img/refresh.png" alt="cekpulsa" height="24" width="24" /></a>
+<!--Untuk Stop Service-->
+<!--<a href="cekpulsa/modem11.php" onclick="javascript:void window.open('cekpulsa/modem11.php','1421757992183','width=400,height=200,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=0,top=0');return false;"><img src="img/refresh.png" alt="cekpulsa" height="24" width="24" /></a>-->
+<br />
+<hr size=2px color="blue" />
+
+
+<b>Restart All Service</b><br/>
+<!--Untuk Restart All Service Service-->
+<button type="button" href="restart.php" onclick="javascript:void window.open('restart.php','1421757992183','width=400,height=200,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=0,top=0');return false;">Restart</button>
+<!--Untuk Stop All Service Service-->
+<button type="button" href="stop.php" onclick="javascript:void window.open('stop.php','1421757992183','width=400,height=200,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=0,top=0');return false;">Stop All Service</button>
+<!--Untuk Cekpulsa all -->
+<button type="button" href="cekpulsa.php" onclick="javascript:void window.open('cekpulsa.php','1421757992183','width=400,height=200,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=0,top=0');return false;">Cek Pulsa Semua Modem</button>
+<!--Untuk cek All Service -->
+<button type="button" href="status.php" onclick="javascript:void window.open('status.php','1421757992183','width=400,height=200,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=0,top=0');return false;">Cek Status Gammu</button>
+<!--Untuk Cek Pulsa semua modem-->
+
+<!--Untuk Cek Pulsa semua modem-->
+<hr size=2px color="blue" />
+
+
+
+</body>
+</html>
+
